a87eb71119c85af6955c4f7fd37c51d35cdd70a1
Richard Croker <ra.croker@gmail.com>
2024-08-05 20:09:05 +0100

    Update streamlit_app.py
    add secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,10 @@ st.set_page_config(layout="wide")
 st.title("OpenPrescribing measures tracker")
 
 def review_months(review_date):
-    # Get the current date
     current_date = datetime.now()
-
-    # Calculate the difference using relativedelta
     difference = relativedelta(review_date, current_date)
-
-    # Calculate the total number of months
     total_months = difference.years * 12 + difference.months
-
-    if total_months < 0:
-        total_months = 0
-
-    return int(total_months)
+    return max(int(total_months), 0)
 
 def style_based_on_next_review(row):
     next_review = row['next_review_months']
@@ -36,80 +27,58 @@ def style_based_on_next_review(row):
     return [''] * len(row)
 
 def email_to_name(email):
-    # Extract the part before the "@" symbol
     local_part = email.split('@')[0]
-    # Split by "."
     parts = local_part.split('.')
-    # Capitalize each part
     capitalized_parts = [part.capitalize() for part in parts]
-    # Join with a space
-    name = ' '.join(capitalized_parts)
-    return name
+    return ' '.join(capitalized_parts)
 
-# Calculate date 6 months from now, and convert to date format
 six_months = datetime.now() + relativedelta(months=6)
 six_months = six_months.date()
 
-# Get data from OpenPrescribing measure definitions from Github API
-res = requests.get('https://api.github.com/repos/ebmdatalab/openprescribing/contents/openprescribing/measures/definitions')
+# Read the GitHub token from Streamlit secrets
+github_token = st.secrets["github_token"]
 
-# Check if the request was successful
-if res.status_code == 200:
-    data = res.json()  # Convert the response to JSON
-    
-    # Ensure 'data' is a list
-    if isinstance(data, list):
-        # Create a list to store the normalized JSON data
-        normalized_data = []
-
-        # Iterate through the files, process only .json files
-        for item in data:
-            if isinstance(item, dict) and item.get('name', '').endswith('.json'):
-                url = item['download_url']  # Get the download URL
-                file_data = requests.get(url).json()  # Fetch the JSON data from the URL
-                table_id = item['name'].split('.')[0]  # Add the 'table_id' field
-                authored_by = file_data.get('authored_by', '')
-                if isinstance(authored_by, list):
-                    authored_by = file_data['authored_by'][0]  # Take the first element if authored_by is a list
-
-                measure_name = file_data.get('name', '')  # Get measure name
-                github_url = item['html_url']  # Get GitHub URL
-                next_review = file_data.get('next_review', None)  # Get review date
-                if isinstance(next_review, list):
-                    next_review = file_data['next_review'][0]  # Take the first element if next_review is a list
-                if next_review is not None:
-                    next_review = datetime.strptime(next_review, '%Y-%m-%d').date()  # Turn into date if not blank
-                row = {  # Get data for each row
-                    'measure_name': measure_name,
-                    'authored_by': email_to_name(authored_by),
-                    'next_review': next_review,
-                    'github_url': github_url,
-                    'next_review_months': review_months(next_review)
-                }
-                normalized_data.append(row)
-        # Sort by review date, putting blank dates first
-        normalized_data = sorted(normalized_data, key=lambda x: (x['next_review'] if x['next_review'] is not None else datetime.min.date()))
-
-        # Convert data to a DataFrame
-        df = pd.DataFrame(normalized_data)
-
-        # Widgets to filter data
-        months_filter = st.slider('Select number of months before review date', min_value=int(df['next_review_months'].min()), max_value=int(df['next_review_months'].max()), value=(int(df['next_review_months'].min()), int(df['next_review_months'].max())))
-
-        # Filter data based on the slider
-        filtered_df = df[(df['next_review_months'] >= months_filter[0]) & (df['next_review_months'] <= months_filter[1])]
-
-        # Apply the function to style the DataFrame
-        styled_df = filtered_df.style.apply(style_based_on_next_review, axis=1)
-
-        st.dataframe(
-            styled_df, 
-            hide_index=True,
-            column_config={"github_url":st.column_config.LinkColumn("Github link", display_text="https://github.com/ebmdatalab/openprescribing/blob/main/openprescribing/measures/definitions/(.*?)"), "next_review_months": None}
-        )
-
-    else:
-        st.error("Unexpected data structure returned by the API.")
+if github_token is None:
+    st.error("GitHub token not found in Streamlit secrets.")
 else:
-    st.error(f"Failed to retrieve data. Status code: {res.status_code}")
+    headers = {'Authorization': f'token {github_token}'}
+    res = requests.get('https://api.github.com/repos/ebmdatalab/openprescribing/contents/openprescribing/measures/definitions', headers=headers)
 
+    if res.status_code == 200:
+        data = res.json()
+        if isinstance(data, list):
+            normalized_data = []
+            for item in data:
+                if isinstance(item, dict) and item.get('name', '').endswith('.json'):
+                    url = item['download_url']
+                    file_data = requests.get(url).json()
+                    table_id = item['name'].split('.')[0]
+                    authored_by = file_data.get('authored_by', '')
+                    if isinstance(authored_by, list):
+                        authored_by = file_data['authored_by'][0]
+
+                    measure_name = file_data.get('name', '')
+                    github_url = item['html_url']
+                    next_review = file_data.get('next_review', None)
+                    if isinstance(next_review, list):
+                        next_review = file_data['next_review'][0]
+                    if next_review is not None:
+                        next_review = datetime.strptime(next_review, '%Y-%m-%d').date()
+                    row = {
+                        'measure_name': measure_name,
+                        'authored_by': email_to_name(authored_by),
+                        'next_review': next_review,
+                        'github_url': github_url,
+                        'next_review_months': review_months(next_review)
+                    }
+                    normalized_data.append(row)
+            normalized_data = sorted(normalized_data, key=lambda x: (x['next_review'] if x['next_review'] is not None else datetime.min.date()))
+            df = pd.DataFrame(normalized_data)
+            months_filter = st.slider('Select number of months before review date', min_value=int(df['next_review_months'].min()), max_value=int(df['next_review_months'].max()), value=(int(df['next_review_months'].min()), int(df['next_review_months'].max())))
+            filtered_df = df[(df['next_review_months'] >= months_filter[0]) & (df['next_review_months'] <= months_filter[1])]
+            styled_df = filtered_df.style.apply(style_based_on_next_review, axis=1)
+            st.dataframe(styled_df, hide_index=True, column_config={"github_url": st.column_config.LinkColumn("Github link", display_text="https://github.com/ebmdatalab/openprescribing/blob/main/openprescribing/measures/definitions/(.*?)"), "next_review_months": None})
+        else:
+            st.error("Unexpected data structure returned by the API.")
+    else:
+        st.error(f"Failed to retrieve data. Status code: {res.status_code}")
